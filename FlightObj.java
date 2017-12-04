fb60493fc489ba719c5639227b104337f5144c61
shaotsu <mkunjo121@gmail.com>
2017-12-03 23:04:07 -0500

    Update FlightObj.java
    Flight object, departure & arrival time calculation, flight generation, and adding to arrayList

--- FlightObj.java
+++ FlightObj.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class FlightObj {
 	String airDepart; //airport that passengers are departing from
 	String airArrival; //airport that passengers are flying to
@@ -8,7 +10,9 @@ public class FlightObj {
 	int flightNumber; //Flight Number
 
 	//Constructors
-	FlightObj (String _airDepart, String _airArrival, int _numSeats, String _gate, double _departTime, double _arrivalTime, int _flightNumber) {
+	FlightObj(){
+	}
+	FlightObj(String _airDepart, String _airArrival, int _numSeats, String _gate, double _departTime, double _arrivalTime, int _flightNumber) {
 		airDepart =  _airDepart; //airport that passengers are departing from
 		airArrival =  _airArrival;
 		numSeats =  _numSeats;
@@ -57,10 +61,46 @@ public class FlightObj {
 	public void setGate(String gate){ 
 		this.gate = gate;
 	}
-	public void setDepartTime(int departTime){ 
+	public void setDepartTime(double departTime){ 
 		this.departTime =  departTime; 
 	}
-	public void setArrivalTime(int arrivalTime){ 
+	public void setArrivalTime(double arrivalTime){ 
 		this.arrivalTime =  arrivalTime; 
 	}
+	
+	//MAIN METHOD
+	public static void main(String[] args){
+	String[] arrivalAirports = {"MIAMI","NASHVILLE","CHARLOTTE","CHARLESTON","RICHMOND","NEWORLEANS","JACKSON"};
+	String depAirport = "ATLANTA";
+	int seats = 250;
+	int[] flightNumbs = {1017, 2017, 3017, 4017, 5017, 6017, 7017};
+	String gate = "C420";
+	//--------------------------------------------
+    //Departure and arrival time calculation
+	//--------------------------------------------
+    double[] startDeparture = new double[7];
+    startDeparture[0] = 6.0;
+    double[] arrivalTimes = new double[7];
+    double[] flightTime = {2.0,1.1,1.25,1.17,1.5,1.6,1.0};
+    for(int i=0;i == arrivalAirports.length;i++){
+      arrivalTimes[i] = (startDeparture[i] + flightTime[i]);
+      startDeparture[i+1] = startDeparture[i] + 2.0;  
+    }
+    
+    //--------------------------------------------
+    //Flight Generation and adding to ArrayList
+    //--------------------------------------------
+    FlightObj[] trip = new FlightObj[7];
+    ArrayList<FlightObj> weeklyFlights = new ArrayList<FlightObj>();
+    for(int i=0;i == arrivalAirports.length;i++){
+    		trip[i].airDepart = depAirport;
+    		trip[i].airArrival = arrivalAirports[i];
+    		trip[i].departTime = startDeparture[i];
+    		trip[i].arrivalTime = arrivalTimes[i];
+    		trip[i].numSeats = seats;
+    		trip[i].gate = gate;
+    		trip[i].flightNumber = flightNumbs[i];
+    		weeklyFlights.add(trip[i]);
+    }
+  }
 }
